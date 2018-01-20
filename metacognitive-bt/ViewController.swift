6b9b4fb4d59b1d6a0cc688952f83691cca97de0c
allison <allielu.allison@gmail.com>
2018-01-20 10:23:37 -0600

    hello world

--- metacognitive-bt/ViewController.swift
+++ metacognitive-bt/ViewController.swift
@@ -12,6 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        label.text = "Hello World"
+        view.addSubview(label)
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
