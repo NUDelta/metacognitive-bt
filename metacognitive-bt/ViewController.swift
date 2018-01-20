af41e12306b69aa9d33b9e2df79457a4b2361f22
allison <allielu.allison@gmail.com>
2018-01-20 10:02:04 -0600

    Initial Commit

---
+++ metacognitive-bt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  metacognitive-bt
+//
+//  Created by Allison on 1/20/18.
+//  Copyright © 2018 DTR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
